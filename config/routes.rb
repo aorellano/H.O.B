085b6200c7a7269c1ea3a436878448bec54701ae
aorellano <al.orellano23@gmail.com>
2020-05-20 01:21:40 -0500

    nav bar items created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'home_pages/welcome'
+  get 'welcome', to: 'home_pages#welcome', as: 'welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
